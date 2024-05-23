018c420ddd8bba76e2169d810bac83f6811b835f
heesu21 <gmltncpfl1@naver.com>
2024-05-23 11:30:15 +0900

    feat: fifth

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //문제1
 const persons = [
-{ name: "철수", age: 19, gender: "남" },
-{ name: "짱구", age: 20, gender: "남" },
-{ name: "유리", age: 21, gender: "여" },
+  { name: "철수", age: 19, gender: "남" },
+  { name: "짱구", age: 20, gender: "남" },
+  { name: "유리", age: 21, gender: "여" },
 ];
 
 //console.log(person);
@@ -21,41 +21,70 @@ persons.forEach(function (person) {
 
 //문제3
 const person = persons.find((person) => {
-  return person.gender === "여"
+  return person.gender === "여";
 });
-  console.log(person);
+console.log(person);
 
 //문제4
 const result = persons.filter((person) => {
-    return person.gender === "남"
-  });
-  
-  console.log(result);
+  return person.gender === "남";
+});
+
+console.log(result);
 
 //문제5 - 기존 데이터 확인 튜터님한테 가기
 const plus = persons.map((person) => {
   return {
     name: person.name,
     age: person.age + 10,
-    gender: person.gender ,
-  }
-})
-  console.log(plus);
+    gender: person.gender,
+  };
+});
+console.log(plus);
 
 //문제6
-const newPersons = [...persons]
-  newPersons.sort((a,b) => {
-    return b.age - a.age
-  });
-  
-  console.log(newPersons);
+const newPersons = [...persons];
+newPersons.sort((a, b) => {
+  return b.age - a.age;
+});
 
-  //문제7
-  const user = {
+console.log(newPersons);
+
+//문제7
+const user = [
+  {
     이름: "병수",
     나이: 20,
     주소: "시골",
-  }
+  },
+];
 
-  const {이름,나이,주소} = user;
-  console.log(user);
+//const { 이름, 나이, 주소 } = user;
+//console.log(user);
+
+//문제8
+localStorage.setItem("user", JSON.stringify(user));
+
+console.log(user);
+
+const num = JSON.parse(localStorage.getItem("user"));
+
+const newUser = num.map((user) => {
+  if (user.나이 === 20) {
+    return {
+      ...user,
+      나이: 50,
+    };
+  }
+  return user;
+});
+localStorage.setItem("user", JSON.stringify(newUser));
+
+localStorage.removeItem("user");
+
+//문제9
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+console.log(fetch);
